32b83f6aa91211781e3f687b6f8cc04c471e3636
asafarviv55 <asaf.arviv@gmail.com>
2022-11-28 14:02:57 +0200

    v1.6

--- WebApp/wwwroot/app-jQuery.js
+++ WebApp/wwwroot/app-jQuery.js
@@ -7,20 +7,7 @@ $(document).ready(function(){
 	}
 
 
-	$("#addrow").on("click", function () {
-		var newRow = $("<tr id='newRow'>");
-		var cols = "";
-		cols += '<td><input type="text" class="form-control" value="" id="newCode" name="code"' + counter + '"/></td>';
-		cols += '<td><input type="text" class="form-control" value="" id="newName" name="name"' + counter + '"/></td>';
-		cols += '<td><input type="text" class="form-control"  value="" id="newDescription" name="description"' + counter + '"/></td>';
-		cols += '<td></td>';
-
-		cols += '<td><input type="button" class="ibtnDel btn btn-md btn-danger "  value="Delete"></td>';
-
-		newRow.append(cols);
-		$("#myTable").append(newRow);
-		counter++;
-	});
+	 
 
 
 	$("#myTable").on("click", ".ibtnDel", function (event) {
